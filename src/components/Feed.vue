b493af195836c45efc1d6ef7626b509b3a39ae9f
mdidius <morphydidius@gmail.com>
2024-05-05 23:22:35 +0300

    feed marking

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,14 +1,82 @@
 <template>
-    <div>Feed {{ feedApiUrl }} {{ type }}</div>
+    <div>
+        <div v-if="isLoading">Loading...</div>
+
+        <div v-if="error">Error :(</div>
+
+        <div v-if="feed">
+            <div
+                 v-for="(article, index) in feed.articles"
+                 :key="index"
+                 class="article-preview"
+            >
+                <div class="article-meta">
+                    <router-link
+                        :to="{
+                            name: 'userProfile',
+                            params: {
+                                slug: article.author.username,
+                            }
+                        }"
+                    >
+                        <img :src="article.author.image" alt="#">
+                    </router-link>
+
+                    <div class="info">
+                        <router-link
+                            :to="{
+                                name: 'userProfile',
+                                params: {
+                                    slug: article.author.username,
+                                }
+                            }"
+                            class="author"
+                        >
+                            {{ article.author.username }}
+                        </router-link>
+
+                        <span class="date">
+                            {{ article.createdAt }}
+                        </span>
+                    </div>
+
+                    <div class="pull-xs-right">
+                        ADD TO FAVOURITES
+                    </div>
+                </div>
+
+                <router-link
+                    :to="{
+                        name: 'article',
+                        params: {
+                            slug: article.slug,
+                        }
+                    }"
+                    class="preview-link"
+                >
+                    <h1>{{ article.title }}</h1>
+                    <p>{{ article.description }}</p>
+                    <span>Read more...</span>
+                </router-link>
+            </div>
+            PAGINATION
+        </div>
+    </div>
 </template>
 
 <script>
 import feedApiUrls from '@/maps/feedApiUrls';
 import { actionTypes } from '@/store/modules/feed';
+import { mapState } from 'vuex';
 
 export default {
     name: 'PfFeed',
     computed: {
+        ...mapState({
+            isLoading: state => state.feed.isLoading,
+            feed: state => state.feed.data,
+            error: state => state.feed.error,
+        }),
         feedApiUrl() {
             return feedApiUrls[this.type] || feedApiUrls['global'];
         },
--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,66 @@ import PostRegistration from '@/views/Registration';
 import PostLogin from '@/views/Login';
 
 const routes = [
-  {
-    path: '/',
-    name: 'globalFeed',
-    component: GlobalFeed,
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: PostLogin,
-  },
-  {
-    path: '/registration',
-    name: 'registration',
-    component: PostRegistration,
-  },
-  {
-    path: '/create-article',
-    name: 'createArticle',
-    component: PostLogin,
-  },
-  {
-    path: '/settings',
-    name: 'settings',
-    component: PostLogin,
-  },
-  {
-    path: '/user',
-    name: 'userProfile',
-    component: PostLogin,
-  }
+    {
+        path: '/',
+        name: 'globalFeed',
+        component: GlobalFeed,
+    },
+    {
+        path: '/feed',
+        name: 'yourFeed',
+        component: GlobalFeed,
+    },
+    {
+        path: '/tags/:slug',
+        name: 'tag',
+        component: GlobalFeed,
+    },
+    {
+        path: '/articles/new',
+        name: 'createArticle',
+        component: GlobalFeed,
+    },
+    {
+        path: '/articles/:slug',
+        name: 'article',
+        component: GlobalFeed,
+    },
+    {
+        path: '/articles/:slug/edit',
+        name: 'editArticle',
+        component: GlobalFeed,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: PostLogin,
+    },
+    {
+        path: '/registration',
+        name: 'registration',
+        component: PostRegistration,
+    },
+    {
+        path: '/settings',
+        name: 'settings',
+        component: PostLogin,
+    },
+    {
+        path: '/profiles/:slug',
+        name: 'userProfile',
+        component: PostLogin,
+    },
+    {
+        path: '/profiles/:slug/favourites',
+        name: 'userProfileFavourites',
+        component: PostLogin,
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
